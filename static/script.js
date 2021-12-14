99d5c1f5c6a4d3b1707a85582a93d6a70b85461b
AustinNear <46238739+AustinNear@users.noreply.github.com>
2021-12-14 12:54:21 -0800

    timer

--- static/script.js
+++ static/script.js
@@ -96,7 +96,7 @@ function getTime() {
         endTime()
     }
     else
-        return Math.floor(60-((new Date() - startTime)/1000))
+        return Math.floor(20-((new Date() - startTime)/1000))
 }
 
 renderQuote()
